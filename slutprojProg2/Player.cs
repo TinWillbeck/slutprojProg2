71967dd4e353bf8d76363764922a8ed59964c06b
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2024-03-21 14:33:45 +0100

    kommenterat

--- slutprojProg2/Player.cs
+++ slutprojProg2/Player.cs
@@ -13,27 +13,53 @@ public class Player
     float rotationSpeed = 4f;
     Vector2 speedVector = Vector2.Zero;
     float friction = 1.5f;
+    // konstruktor som gör att man kan sätta motor och annat när man instansierar player
     public Player(Engine engine)
     {
+        // sätter bilens accelerations och toppfartsvärden till motorns respektive värden
         acceleration = engine.GetAcceleration();
         maxSpeed = engine.GetSpeed();
 
     }
-
+    // metod som har hand om bilens rörelse framåt
     private void Movement()
     {
+        // om man håller in W så accelererar bilen upp till sin toppfart
         if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && speed < maxSpeed)
         {
             speed += acceleration;
         }
-
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && speed > -40)
+        // om man håller in S så accelererar bilen bakåt till en tredjededel av sin maxfart
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && speed > maxSpeed / 3)
         {
             speed -= acceleration;
         }
 
+        GetRotation();
+        //carDirection X är cosinus av carrotation omvandlat till radianer
+        //carDirection Y är sinus av carrotation omvandlat till radianer
+        carDirection.X = MathF.Cos((MathF.PI / 180) * carRotation);
+        carDirection.Y = MathF.Sin((MathF.PI / 180) * carRotation);
+
+        // speedvector X och Y är respektive carDirection X oxh Y multiplicerad med speed
+        speedVector.X = carDirection.X * speed;
+        speedVector.Y = carDirection.Y * speed;
+
+        // debug funktion som teleporterar bilen till mitten av skärmen
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+        {
+            car.X = 400;
+            car.Y = 400;
+        }
+
+    }
+    // metod som roterar bilen
+    void GetRotation()
+    {
+        // kollar om bilen rör på sig
         if (speedVector.Length() > 0)
         {
+            // om man håller in A roteras bilen åt vänster
             if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
             {
                 carRotation -= rotationSpeed;
@@ -42,6 +68,7 @@ public class Player
                     carRotation += 360;
                 }
             }
+            // om man håller in D så roterar bilen åt höger
             if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
             {
                 carRotation += rotationSpeed;
@@ -51,31 +78,12 @@ public class Player
                 }
             }
         }
-        if (speed > 0)
-        {
-            speed -= friction;
-        }
-        else if (speed < 0)
-        {
-            speed += friction;
-        }
-
-        carDirection.Y = MathF.Sin((MathF.PI / 180) * carRotation);
-        carDirection.X = MathF.Cos((MathF.PI / 180) * carRotation);
-
-        speedVector.X = carDirection.X * speed;
-        speedVector.Y = carDirection.Y * speed;
-
-
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
-        {
-            car.X = 400;
-            car.Y = 400;
-        }
     }
 
+    // metod som räknar ut bilens hörn och lägger in dem i en lista med vektorer
     public Vector2[] GetCorners()
     {
+        // vektorns X 
         Vector2 topRight = new()
         {
             X = car.X + carOrigin.Y / 1.5f * MathF.Cos(carRotation) - carOrigin.X / 1.5f * MathF.Sin(carRotation),
@@ -97,7 +105,7 @@ public class Player
             Y = car.Y - carOrigin.Y / 1.5f * MathF.Sin(carRotation) - carOrigin.X / 1.5f * MathF.Cos(carRotation)
         };
 
-        return new Vector2[] {topLeft, topRight, bottomRight, bottomLeft};        
+        return new Vector2[] { topLeft, topRight, bottomRight, bottomLeft };
     }
     public void Collision()
     {
@@ -124,6 +132,14 @@ public class Player
         Movement();
         car.X += speedVector.X * Raylib.GetFrameTime();
         car.Y += speedVector.Y * Raylib.GetFrameTime();
+        if (speed > 0)
+        {
+            speed -= friction;
+        }
+        else if (speed < 0)
+        {
+            speed += friction;
+        }
 
     }
 }
